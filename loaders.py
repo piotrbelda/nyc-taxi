72cc5f9cabae1925f61af378933d3a5c204bad4f
Piotr Bełda <piotrek.belda@wp.pl>
2024-09-04 19:58:32 +0200

    parse taxi url

--- loaders.py
+++ loaders.py
@@ -1,13 +1,20 @@
+import re
+from typing import Tuple
+
 import httpx
 from scrapy import Selector
 
 TAXI_DATA_PAGE_URL = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
 
-response = httpx.get(TAXI_DATA_PAGE_URL, timeout=10)
-selector = Selector(text = response.content)
-file_hyperlink = selector.xpath('//div[@class="faq-answers"]//li//a/@href').get()
 
-def get_latest_taxi_file_hyperlink():
-    pass
+def sort_by_year_month(s: str) -> Tuple[int, int]:
+    search = re.search(r"(\d{4})-(\d{2})", s)
+    return int(search.group(1)), int(search.group(2))
 
-breakpoint()
+
+def get_latest_taxi_file_hyperlink() -> list[str]:
+    response = httpx.get(TAXI_DATA_PAGE_URL, timeout=10)
+    selector = Selector(text = response.content)
+    hyperlinks = selector.xpath('//div[@class="faq-answers"]//li//a/@href').getall()
+    hyperlinks = sorted(hyperlinks, key=sort_by_year_month)
+    return hyperlinks[-1]
