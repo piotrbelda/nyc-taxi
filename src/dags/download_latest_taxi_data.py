b45bc105898070d109787b2f755d32ac46c90b11
Piotr Bełda <piotrek.belda@wp.pl>
2024-09-15 10:46:38 +0200

    file sensor

--- src/dags/download_latest_taxi_data.py
+++ src/dags/download_latest_taxi_data.py
@@ -1,11 +1,15 @@
 import re
+import tempfile
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Tuple
 
 import httpx
 from airflow.models.dag import DAG
+from airflow.models.connection import Connection
 from airflow.operators.bash import BashOperator
+from airflow.sensors.filesystem import FileSensor
+from airflow.utils.session import provide_session
 from scrapy import Selector
 
 TAXI_DATA_PAGE_URL = 'https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
@@ -24,6 +28,20 @@ def get_latest_taxi_file_hyperlink() -> list[str]:
     return hyperlinks[-1]
 
 
+tmp_dir = tempfile.gettempdir()
+
+@provide_session
+def create_connection(session=None):
+    conn = Connection(
+        conn_id='file_path',
+        conn_type='fs',
+        host=tmp_dir,
+    )
+
+    if not session.query(Connection).filter(Connection.conn_id == conn.conn_id).first():
+        session.add(conn)
+        session.commit()
+
 with DAG(
     dag_id='test',
     default_args={
@@ -41,10 +59,19 @@ with DAG(
     tags=["example"],
 ):
     latest_file_url = get_latest_taxi_file_hyperlink()
-    file_name = Path(latest_file_url).name
+    file_path = Path(tmp_dir) / Path(latest_file_url).name
+
     download_data = BashOperator(
         task_id='download_data',
-        bash_command=f'curl {latest_file_url} -o /tmp/{file_name}',
+        bash_command=f'curl {latest_file_url} -o {str(file_path)}',
+    )
+
+    create_connection()
+
+    file_sensor = FileSensor(
+        task_id='file_sensor',
+        filepath=str(file_path.parent),
+        fs_conn_id='file_path',
     )
 
     sleep = BashOperator(
@@ -52,5 +79,5 @@ with DAG(
         bash_command=f'sleep 1',
     )
 
-    download_data >> sleep
+    download_data >> file_sensor >> sleep
  
