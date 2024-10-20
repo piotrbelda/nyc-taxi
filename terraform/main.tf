c8803efea113c3545ed832e6a779f8bb66da05e8
Piotr Bełda <piotrek.belda@wp.pl>
2024-10-20 11:20:56 +0200

    terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "mlflow-taxi-test"
+
+  tags = {
+    Name        = "MLflow Taxi bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "test_bucket_access_block" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+  ignore_public_acls      = false
+}
