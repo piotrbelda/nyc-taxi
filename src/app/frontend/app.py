f6a2e5fdddee583469bc7eabf39c4a93274ecbcf
Piotr Bełda <piotrek.belda@wp.pl>
2024-10-25 23:04:14 +0200

    frontend setup

--- src/app/frontend/app.py
+++ src/app/frontend/app.py
@@ -1,3 +1,15 @@
+import pandas as pd
 import streamlit as st
 
+from taxi_db.model import Location
+from taxi_db.utils.session import TaxiSession
+
+session = TaxiSession().session
+
+df = pd.read_sql(session.query(Location).statement, con=session.get_bind())
+d = df.groupby(Location.borough.name)[Location.zone.name].apply(lambda x: x.tolist()).to_dict()
+
 st.title('NYC Taxi Trip Predictor')
+borough = st.selectbox('Borough', d.keys())
+if borough:
+    zone = st.selectbox('Zone', d[borough])
