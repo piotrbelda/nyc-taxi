9c5fc23037b043e6df2be9aed2d2b7ee4cd064f3
Piotr Bełda <piotrek.belda@wp.pl>
2025-02-24 18:02:37 +0100

    add road table

---
+++ alembic/versions/206df03cc924_init_road_table.py
@@ -0,0 +1,38 @@
+"""init road table
+
+Revision ID: 206df03cc924
+Revises: ba7b595e452d
+Create Date: 2025-02-24 16:54:10.286691
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+from geoalchemy2.types import Geometry
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '206df03cc924'
+down_revision: Union[str, None] = 'ba7b595e452d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'road',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.Text(), nullable=False),
+        sa.Column('type', sa.VARCHAR(length=1), nullable=True),
+        sa.Column('geom', Geometry(geometry_type='LINESTRING', srid=4326, from_text='ST_GeomFromEWKT', name='geometry', spatial_index=False), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('id'),
+        schema='data',
+    )
+    op.create_index('idx_road_geom', 'road', ['geom'], unique=False, schema='data', postgresql_using='gist')
+
+
+def downgrade() -> None:
+    op.drop_index('idx_road_geom', table_name='road', schema='data', postgresql_using='gist')
+    op.drop_table('road', schema='data')
--- src/common/taxi-db/src/taxi_db/model/__init__.py
+++ src/common/taxi-db/src/taxi_db/model/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base
 from .location import Location
+from .road import Road
 from .trip import Trip
